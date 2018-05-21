eb484faca43543f85b9c63137d3c10e21115d0a5
SANDEEP KP <sandeepkp07@gmail.com>
2018-05-21 15:50:32 +0530

    Update twitter.py

--- twitter/twitter.py
+++ twitter/twitter.py
@@ -10,7 +10,7 @@ def tweets(username):
     user_timeline= "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name="+username+"&count=10"
     USER=AUTH.get(user_timeline)
     if(USER.status_code == 404):
-     print "error"
+     print "Please check username!!!!"
     else:    
      print "TWEETS:\n" 
      TWEETS = json.loads(USER.text)
