84cd05f1d03dfe08d08de48bc0d15316e082f5c6
SANDEEP KP <sandeepkp07@gmail.com>
2018-05-29 20:35:17 +0530

    currency_converter code

---
+++ currency_converter/currency_converter.py
@@ -0,0 +1,15 @@
+import requests
+import ast
+
+def converter(cfrm,cto,amt):
+    res=requests.get("https://api.fixer.io/latest?base="+cfrm)
+    text = ast.literal_eval(res.text)
+    rates = text["rates"]
+    print "YOU CAN CONVERT IT TO:\n",rates.keys()
+    amount = (rates[cto])*(float(amt))
+    print str(amount)
+print("Enter the currency name in capital letters(3 letters)")
+cfrm = raw_input("convert from:")
+cto =raw_input("convert to")
+amt = input("amount:")
+converter(cfrm,cto,amt)
