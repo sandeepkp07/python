a53eac48d56c4f16c359c3322d77bffb9d116deb
SANDEEP KP <sandeepkp07@gmail.com>
2018-06-04 22:49:08 +0530

    Update rssreader.py

--- rssreader/rssreader.py
+++ rssreader/rssreader.py
@@ -1,7 +1,7 @@
 import feedparser
-r=raw_input("enter url")
-f=feedparser.parse(r)
+url=raw_input("enter url")
+Data=feedparser.parse(r)
 i=1
-for post in f.entries:
+for post in Data.entries:
     print i,"\n",post.title+"\n"+post.published+":"+post.summary+"\n"+"#Link:"+post.link
     i=i+1
